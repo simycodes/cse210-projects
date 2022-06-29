8625d1dc6f13f64f29ddb67650e3eb25e3260197
simycodes <simycodes@gmail.com>
2022-06-29 23:36:58 +0200

    added snake game

---
+++ Snake game/game/scripting/move_actors_action.py
@@ -0,0 +1,29 @@
+from game.scripting.action import Action
+
+
+# TODO: Implement MoveActorsAction class here! 
+
+# Override the execute(cast, script) method as follows:
+# 1) get all the actors from the cast
+# 2) loop through the actors
+# 3) call the move_next() method on each actor
+
+
+class MoveActorsAction(Action):
+    """
+    An update action that moves all the actors.
+    
+    The responsibility of MoveActorsAction is to move all the actors that have a velocity greater
+    than zero.
+    """
+
+    def execute(self, cast, script):
+        """Executes the move actors action.
+
+        Args:
+            cast (Cast): The cast of Actors in the game.
+            script (Script): The script of Actions in the game.
+        """
+        actors = cast.get_all_actors()
+        for actor in actors:
+            actor.move_next()
